5592f0290573edae5f3427eded57993d370fd99f
Jan Niklas Rose <janniklas.rose@gmail.com>
2020-08-26 23:30:16 +0200

    Add setup.py to make cell_packing installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='cell_packing',
+      version='0.0',
+      description='Cell Packing',
+      author='Jan Niklas Rose',
+      author_email='janniklas.rose@gmail.com',
+      url='https://gitlab.com/janniklasrose/cell-packing',
+      packages=['polypacker'],
+     )
